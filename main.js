521d3356d74898f80044025dc6befc9ff4e14bc7
Soumya1002 <soumyasenthil1002@gmail.com>
2024-01-11 11:31:17 +0530

    styled form and console.log details

---
+++ main.js
@@ -0,0 +1,15 @@
+function addUser(){
+
+    console.log("in");          
+    
+      var name=document.getElementById('username').value;
+       var password=document.getElementById('password').value;
+        var  emailid= document.getElementById('email').value;
+    
+        console.log(name);
+        console.log(password);   
+        console.log(emailid);       
+
+    return false;
+
+};
